c07fbe84c7d82407e3c81308cadb7a9a17bc665f
Lilly Tong <lilly.tong@mail.mcgill.ca>
2015-02-15 00:55:34 -0500

    finished test cases

---
+++ src/tests/CalculatorTests.java
@@ -0,0 +1,174 @@
+package tests;
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import util.Calculator;
+
+public class CalculatorTests{
+
+	@Test 
+	public void cToP_returns_double_array() throws Exception
+	{
+		double[] result=Calculator.cartesianToPolar(3,4);
+		Assert.assertNotNull(result);
+		
+	}
+	
+	@Test 
+	public void cToP_inputs_are_zero() throws Exception
+	{
+		boolean thrown = false;
+		try {
+		    Calculator.cartesianToPolar(0,0);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+
+		  Assert.assertTrue(thrown);
+	}
+	
+	@Test 
+	public void cToP_inputs_are_nonzero() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.cartesianToPolar(3.0,4.0);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(result[0]==5 && (Math.abs(result[1]-Math.atan(4.0/3.0))<0.1));
+	}
+	
+	@Test 
+	public void cToP_contains_negative_input() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.cartesianToPolar(3.0,-4.0);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(result[0]==5 && (Math.abs(result[1]-Math.atan(-4.0/3.0))<0.1));
+	}
+	
+	@Test 
+	public void cToP_inputs_too_large() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.cartesianToPolar(10000,-10000);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(thrown);
+	}
+	
+	@Test 
+	public void cToP_inputs_too_small() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.cartesianToPolar(0.01,0.01);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(thrown);
+	}
+	
+	@Test 
+	public void pToC_returns_double_array() throws Exception
+	{
+		double[] result=Calculator.polarToCartesian(3,0.5);
+		Assert.assertNotNull(result);
+		
+	}
+	
+	@Test 
+	public void pToC_inputs_too_large() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.polarToCartesian(10000,-10000);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(thrown);
+	}
+	
+	@Test 
+	public void pToC_inputs_are_nonzero() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.polarToCartesian(5,Math.atan(4.0/3.0));
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(Math.abs(3-result[0])<0.01 && Math.abs(4-result[1])<0.01);
+	}
+	
+	@Test 
+	public void add2vectors_returns_double_array() throws Exception
+	{
+		double[] result=Calculator.add2vectors(3,4,5,6);
+		Assert.assertNotNull(result);
+	}
+	
+	@Test 
+	public void add2vectors_regular_input() throws Exception
+	{
+		double[] result=Calculator.add2vectors(3,4,5,6);
+		Assert.assertTrue(result[0]==8 && result[1]==10);
+	}
+	
+	@Test 
+	public void add2vectors_inputs_too_large() throws Exception
+	{
+		boolean thrown = false;
+		double[] result={-1,-1};
+		try {
+		    result = Calculator.add2vectors(10000,-10000,10000,-10000);
+		  } catch (Exception e) {
+		    thrown = true;
+		  }
+		
+		  Assert.assertTrue(thrown);
+	}
+	
+	@Test 
+	public void add3vectors_returns_double_array() throws Exception
+	{
+		double[] result=Calculator.add3vectors(3,4,5,6,7,8);
+		Assert.assertNotNull(result);
+	}
+	
+	@Test 
+	public void scalarProduct_regular_input()
+	{
+		double result=Calculator.scalarProduct(3,4,5,6);
+		Assert.assertTrue(result == 3*5+4*6);
+	}
+	
+	@Test 
+	public void vectorProduct_regular_input()
+	{
+		double result=Calculator.vectorProduct(3,4,5,6);
+		Assert.assertTrue(result == 3*6-4*5);
+	}
+	
+	
+}
--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -2,27 +2,51 @@ package util;
 
 public class Calculator {
 
-	public static double[] cartesianToPolar(double x, double y)
+	public static double[] cartesianToPolar(double x, double y) throws Exception
 	{
+		
+		if (x==0 || y==0)
+		{
+			throw new Exception("One or more inputs are zero!");
+		}
+		
+		else if (Math.abs(x)>=10000 || Math.abs(y)>=10000)
+		{
+			throw new Exception("Inputs too large!");
+		}
+		
+		else if (Math.abs(x)<=0.01 || Math.abs(y)<=0.01)
+		{
+			throw new Exception("Inputs too small!");
+		}
+		
 		double[] result = new double[2]; 
 		result[0]=Math.sqrt(Math.pow(x, 2)+Math.pow(y, 2));
 		result[1]=Math.atan(y/x);
 		return result; //result[0]=radius, result[1]=angle
 	}
 	
-	public static double[] polarToCartesian(double r, double theta)
+	public static double[] polarToCartesian(double r, double theta) throws Exception
 	{
-		//assume theta is in DEGREES
+		//theta in radians
+		if (Math.abs(r)>=10000 || Math.abs(theta)>=10000)
+		{
+			throw new Exception("Inputs too large!");
+		}
+		
 		double[] result = new double[2]; 
-		double radians=Math.toRadians(theta); //convert angle into radians
-		result[0]=r*Math.cos(radians);
-		result[1]=r*Math.sin(radians);
+		result[0]=r*Math.cos(theta); 
+		result[1]=r*Math.sin(theta);
 		return result; //result[0]=x, result[1]=y
 	}
 	
-	//all methods below intended for Cartesian coordinates!
-	public static double[] add2vectors(double x1, double y1, double x2, double y2)
+	public static double[] add2vectors(double x1, double y1, double x2, double y2) throws Exception
 	{
+		if (Math.abs(x1)>=10000 || Math.abs(y1)>=10000 || Math.abs(x2)>=10000 || Math.abs(y2)>=10000)
+		{
+			throw new Exception("Inputs too large!");
+		}
+		
 		double[] result=new double[2];
 		result[0]=x1+x2;
 		result[1]=y1+y2;
@@ -46,4 +70,7 @@ public class Calculator {
 	{
 		return x1*y2-y1*x2; //this is just the value of the z component! so answer will be (0,0,z)!
 	}
+	
+	
 }
+	
